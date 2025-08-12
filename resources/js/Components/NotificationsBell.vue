1475ad33383c56c05cbada6b381c3eb3285c750c
boey-13 <baoyee13@gmail.com>
2025-08-12 23:59:50 +0800

    ui(notifications): use inline SVG bell icon for notification button

--- resources/js/Components/NotificationsBell.vue
+++ resources/js/Components/NotificationsBell.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
 
 const open = ref(false)
 const items = ref([])
@@ -18,7 +18,11 @@ function toggle() {
 <template>
   <div class="relative">
     <button class="relative" @click="toggle" aria-label="Notifications">
-      <span class="material-icons">notifications</span>
+      <!-- Inline SVG bell icon (no external font needed) -->
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+        <path d="M12 2a6 6 0 00-6 6v3.586l-.707.707A1 1 0 006 14h12a1 1 0 00.707-1.707L18 11.586V8a6 6 0 00-6-6z"/>
+        <path d="M8 16a4 4 0 008 0H8z"/>
+      </svg>
       <span v-if="items.length" class="absolute -top-1 -right-1 bg-red-600 text-white text-[10px] rounded-full px-1">{{ items.length }}</span>
     </button>
     <div v-if="open" class="absolute right-0 mt-2 w-80 bg-white rounded shadow z-50">
@@ -32,6 +36,7 @@ function toggle() {
       </div>
     </div>
   </div>
+  
 </template>
 
 
